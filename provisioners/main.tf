7ecaf6ab344af6efc8a5f803de074f8641d67d77
Pavani <vemasani.pavani@gmail.com>
2024-05-05 21:24:12 +0530

    delete component variable

--- provisioners/main.tf
+++ provisioners/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "instance" {
   ami = var.ami
   instance_type = var.instance_type
   tags = {
-    Name = var.component
+    Name = "demo"
   }
   provisioner "remote-exec" {
     connection {
