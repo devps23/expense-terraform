ff9268ce02ad34e379296c5c9f598812d53a829c
Pavani <vemasani.pavani@gmail.com>
2024-05-06 12:30:05 +0530

    create three resource

--- main.tf
+++ main.tf
@@ -6,3 +6,19 @@ module "frontend" {
   ssh_pass = var.ssh_pass
   env = var.env
 }
+module "backend" {
+  source = "./module/app"
+  instance_type = var.instance_type
+  component = "backend"
+  ssh_user = var.ssh_user
+  ssh_pass = var.ssh_pass
+  env = var.env
+}
+module "mysql" {
+  source = "./module/app"
+  instance_type = var.instance_type
+  component = "mysql"
+  ssh_user = var.ssh_user
+  ssh_pass = var.ssh_pass
+  env = var.env
+}
