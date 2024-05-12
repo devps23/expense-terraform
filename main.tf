194a17ee28572cbd82e5c8bc839fb70b0b49f2b0
Pavani <vemasani.pavani@gmail.com>
2024-05-12 18:30:24 +0530

    decrypt vault..

--- main.tf
+++ main.tf
@@ -1,21 +1,21 @@
-module "frontend" {
-  depends_on = [module.backend]
-  source = "./module/app"
-  instance_type = var.instance_type
-  component = "frontend"
-//  ssh_user = var.ssh_user
-//  ssh_pass = var.ssh_pass
-  env = var.env
-}
-module "backend" {
-  depends_on = [module.mysql]
-  source = "./module/app"
-  instance_type = var.instance_type
-  component = "backend"
-//  ssh_user = var.ssh_user
-//  ssh_pass = var.ssh_pass
-  env = var.env
-}
+//module "frontend" {
+//  depends_on = [module.backend]
+//  source = "./module/app"
+//  instance_type = var.instance_type
+//  component = "frontend"
+////  ssh_user = var.ssh_user
+////  ssh_pass = var.ssh_pass
+//  env = var.env
+//}
+//module "backend" {
+//  depends_on = [module.mysql]
+//  source = "./module/app"
+//  instance_type = var.instance_type
+//  component = "backend"
+////  ssh_user = var.ssh_user
+////  ssh_pass = var.ssh_pass
+//  env = var.env
+//}
 module "mysql" {
   source = "./module/app"
   instance_type = var.instance_type
