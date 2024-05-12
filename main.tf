dd11bbca84ca611f62c8bdad9cfe232a9a1d84ca
Pavani <vemasani.pavani@gmail.com>
2024-05-12 18:42:54 +0530

    run frontend,backend....

--- main.tf
+++ main.tf
@@ -3,8 +3,6 @@ module "frontend" {
   source = "./module/app"
   instance_type = var.instance_type
   component = "frontend"
-//  ssh_user = var.ssh_user
-//  ssh_pass = var.ssh_pass
   env = var.env
 }
 module "backend" {
@@ -12,15 +10,11 @@ module "backend" {
   source = "./module/app"
   instance_type = var.instance_type
   component = "backend"
-//  ssh_user = var.ssh_user
-//  ssh_pass = var.ssh_pass
   env = var.env
 }
 module "mysql" {
   source = "./module/app"
   instance_type = var.instance_type
   component = "mysql"
-//  ssh_user = var.ssh_user
-//  ssh_pass = var.ssh_pass
   env = var.env
 }
