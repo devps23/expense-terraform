70d756bd5e23373284d9ca7989416a341bdd8677
Pavani <vemasani.pavani@gmail.com>
2024-05-14 14:43:54 +0530

    call subnets into ec2 instance

--- module/app/main.tf
+++ module/app/main.tf
@@ -23,8 +23,7 @@ resource "aws_instance" "instance" {
   ami = data.aws_ami.ami.image_id
   instance_type = var.instance_type
   vpc_security_group_ids = [aws_security_group.security.id]
-   count = length(var.subnets)
-   subnet_id = var.subnets[count.index]
+   subnet_id = var.subnets[0]
 
 
   tags = {
