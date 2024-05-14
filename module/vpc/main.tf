374da7a2105a8a72eefb67921448d589270979cb
Pavani <vemasani.pavani@gmail.com>
2024-05-14 13:35:40 +0530

    associate with two subnets

--- module/vpc/main.tf
+++ module/vpc/main.tf
@@ -39,8 +39,10 @@ resource "aws_vpc_peering_connection" "peer" {
   vpc_id = aws_vpc.vpc.id
 }
 resource "aws_route" "entry_route"{
-  route_table_id            = aws_vpc.vpc.main_route_table_id
+  count = length(var.frontend-subnets)
+  route_table_id            = aws_vpc.vpc.main_route_table_id[count.index]
   destination_cidr_block    = var.default_vpc_cidr
   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+
 }
 
