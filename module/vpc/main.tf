f2ba8975a6efbe4e77f49a2cab1ff23cf9c6d11f
Pavani <vemasani.pavani@gmail.com>
2024-05-15 18:07:37 +0530

    connect frontend,backend,mysql private subnets to nat gateway

--- module/vpc/main.tf
+++ module/vpc/main.tf
@@ -115,7 +115,7 @@ resource "aws_route_table" "public_route" {
 
   }
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block = 0.0.0.0/0
     gateway_id = aws_internet_gateway.igw.id
   }
   tags = {
