18cac5d8bb3e1c57121d82574befcaee01c384d6
Pavani <vemasani.pavani@gmail.com>
2024-05-15 14:36:46 +0530

    spot instance

--- module/app/main.tf
+++ module/app/main.tf
@@ -41,7 +41,7 @@ resource "aws_instance" "component" {
 resource "aws_instance" "instance" {
   ami = data.aws_ami.ami.image_id
   instance_type = var.instance_type
-  vpc_security_group_ids = [aws_security_group.security.id]
+
     instance_market_options {
       market_type = "spot"
       spot_options {
