028c3fd55148e55608f2502e5b35552d4d7f2317
Pavani <vemasani.pavani@gmail.com>
2024-05-15 17:51:35 +0530

    1.create natgateway 2.connect public subnets to internet gateway 3. connect public subnets to natgateway

--- module/vpc/main.tf
+++ module/vpc/main.tf
@@ -90,6 +90,36 @@ resource "aws_route_table" "db_route" {
     Name = "db-rt-${count.index}"
   }
 }
+resource "aws_route_table" "public_route" {
+  count = length(var.public-subnets)
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = var.default_vpc_cidr
+    vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+
+  }
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "public-rt-${count.index}"
+  }
+}
+resource "aws_eip" "eip" {
+  count = length(var.public-subnets)
+  domain   = "vpc"
+}
+resource "aws_nat_gateway" "nat" {
+  count = length(var.public-subnets)
+  allocation_id = aws_eip.eip.id
+  subnet_id = aws_subnet.public_subnets[count.index].id
+
+  tags = {
+    Name = "nat-gw"
+  }
+}
 resource "aws_route_table_association" "frontend_ass" {
   count = length(var.frontend-subnets)
   subnet_id      = aws_subnet.frontend_subnets[count.index].id
@@ -105,9 +135,20 @@ resource "aws_route_table_association" "db_ass" {
   subnet_id      = aws_subnet.db_subnets[count.index].id
   route_table_id = aws_route_table.db_route[count.index].id
 }
+resource "aws_route_table_association" "db_ass" {
+  count = length(var.db-subnets)
+  subnet_id      = aws_subnet.db_subnets[count.index].id
+  route_table_id = aws_route_table.db_route[count.index].id
+}
+resource "aws_route_table_association" "public_ass" {
+  count = length(var.public-subnets)
+  subnet_id      = aws_subnet.public_subnets[count.index].id
+  route_table_id = aws_route_table.public_route[count.index].id
+}
 resource "aws_vpc_peering_connection" "peer" {
   peer_vpc_id = var.default_vpc_id
   vpc_id = aws_vpc.vpc.id
+  auto_accept = true
   tags = {
     Name = "peer-${var.env}-new"
   }
