d4a021de3eae6c68097547537150e71734f05f1c
Pavani <vemasani.pavani@gmail.com>
2024-05-20 15:25:35 +0530

    attach target group into register targets

--- module/app/main.tf
+++ module/app/main.tf
@@ -92,7 +92,7 @@ resource "aws_lb" "lb" {
   }
 }
 resource "aws_lb_target_group_attachment" "test" {
-  target_group_arn = aws_lb_target_group.target[0].arn
+  target_group_arn = aws_lb_target_group[0].target.arn
   target_id        = aws_instance.component.id
   port             = var.app_port
 }
