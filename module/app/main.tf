b28dcc10ec741ffc21f08cbf91adc684f14f046d
Pavani <vemasani.pavani@gmail.com>
2024-05-22 19:34:15 +0530

    added from port to port as 0

--- module/app/main.tf
+++ module/app/main.tf
@@ -9,6 +9,12 @@ resource "aws_security_group" "security" {
     cidr_blocks      = ["0.0.0.0/0"]
     }
   ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  ingress {
     from_port        = var.app_port
     to_port          = var.app_port
     protocol         = "-1"
