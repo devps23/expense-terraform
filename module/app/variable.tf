dd906028edc0850af8883dfd815ddf6b6ee6ff16
Pavani <vemasani.pavani@gmail.com>
2024-05-24 19:57:32 +0530

    remove null

--- module/app/variable.tf
+++ module/app/variable.tf
@@ -5,9 +5,7 @@ variable "vpc_id" {}
 variable "subnets" {}
 variable "ssh_user"{}
 variable "ssh_pass"{}
-variable "add_sg_app_port" {
-  default = null
-}
+variable "add_sg_app_port" {}
 variable "lb_tg_group" {
   default = false
 }
