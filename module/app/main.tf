14b204f0bf2ac6cfb6cb85c04e120eb2a4076daf
Pavani <vemasani.pavani@gmail.com>
2024-05-29 10:26:41 +0530

    missed comma to seperate

--- module/app/main.tf
+++ module/app/main.tf
@@ -79,7 +79,7 @@ resource "null_resource" "provisioner" {
     }
     inline = [
       "sudo pip3.11 install ansible",
-      "ansible-pull -i localhost. -U https://github.com/devps23/expense-ansible get_secrets_vault.yml -e env=${var.env} -e componente_name=${var.component} -e vault_token=${var.vault_token}"
+      "ansible-pull -i localhost. -U https://github.com/devps23/expense-ansible get_secrets_vault.yml -e env=${var.env} -e componente_name=${var.component} -e vault_token=${var.vault_token}",
       "ansible-pull -i localhost, -U https://github.com/devps23/expense-ansible expense.yml -e env=${var.env} -e component_name=${var.component} -e @secrets.json -e app.json"
 
     ]
